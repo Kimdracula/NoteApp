c49ad086ce0e890215eb8364087484d1f290289b
Vlad <vlad-ard@ya.ru>
2021-11-28 12:48:44 +0300

    Изменил кнопку добавления заметок и подправил дизайн

---
+++ app/src/main/java/com/homework/homework_6/Constants.java
@@ -0,0 +1,22 @@
+package com.homework.homework_6;
+
+public class Constants {
+    String header;
+    String description;
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

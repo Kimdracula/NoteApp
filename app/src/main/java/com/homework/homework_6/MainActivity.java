c18a052d45f0c1a30343c3b97b3c22e38619ef91
Vlad <vlad-ard@ya.ru>
2021-12-02 18:37:24 +0300

    Сделал главный экран приложения с возможностью отображения Child фрагментов и с возможностю перехода на список заметок.

--- app/src/main/java/com/homework/homework_6/MainActivity.java
+++ app/src/main/java/com/homework/homework_6/MainActivity.java
@@ -8,19 +8,15 @@ import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    FragmentList fragmentList;
-    AddFragment addFragment;
-    NoteFragment noteFragment;
+ StartFragment startFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        fragmentList = new FragmentList();
-        addFragment = new AddFragment();
-        noteFragment = new NoteFragment();
+        startFragment = new StartFragment();
         FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.fragment_container,fragmentList).commit();
+        fragmentManager.beginTransaction().replace(R.id.fragment_container,startFragment).commit();
 
     }
 }

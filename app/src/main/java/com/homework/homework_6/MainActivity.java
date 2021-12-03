9ce8166f62ee17c9131d7e410137265d3d725ecd
Vlad <vlad-ard@ya.ru>
2021-12-03 19:08:50 +0300

    Реализовал переход из Drawer на FragmentAbout На остальные пункты меню сделал заглушки (toast)

--- app/src/main/java/com/homework/homework_6/MainActivity.java
+++ app/src/main/java/com/homework/homework_6/MainActivity.java
@@ -1,16 +1,22 @@
 package com.homework.homework_6;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.app.FragmentTransaction;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
     FragmentList fragmentList;
+    AboutFragment aboutFragment;
 
 
     @Override
@@ -18,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         fragmentList = new FragmentList();
+        aboutFragment = new AboutFragment();
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction().replace(R.id.fragment_container,fragmentList).commit();
 initDrawer();
@@ -25,7 +32,60 @@ initDrawer();
 
     private void initDrawer() {
         DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.nav_about:
+                        openAboutFragment();
+                        return true;
+                    case R.id.nav_settings:
+                        openSettingsFragment();
+                        return true;
+                    case R.id.nav_theme:
+                        openThemeFragment();
+                        return true;
+                    case R.id.nav_share:
+                        openShareFragment();
+                        return true;
+                    case R.id.nav_send:
+                        openSendFragment();
+                        return true;
+                }
+                return false;
+            }
+        });
 
+    }
 
+    private void openSendFragment() {
+        Toast.makeText(this,
+                "Переходим на другой фрагмент", Toast.LENGTH_SHORT).show();
+    }
+
+    private void openShareFragment() {
+        Toast.makeText(this,
+                "Переходим на другой фрагмент", Toast.LENGTH_SHORT).show();
+    }
+
+    private void openThemeFragment() {
+        Toast.makeText(this,
+                "Переходим на другой фрагмент", Toast.LENGTH_SHORT).show();
+    }
+
+    private void openSettingsFragment() {
+        Toast.makeText(this,
+                "Переходим на другой фрагмент", Toast.LENGTH_SHORT).show();
+    }
+
+    private void openAboutFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        androidx.fragment.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container,aboutFragment);
+        fragmentTransaction.addToBackStack("");
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fragmentTransaction.commit();
     }
 }
